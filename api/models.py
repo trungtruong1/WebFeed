c4aa2dcd11c08235ad50d302e83a583f93fc7669
phong260702 <phongdangquoc123@gmail.com>
2024-07-23 16:16:56 -0400

    Define models for user and topic

--- api/models.py
+++ api/models.py
@@ -6,3 +6,30 @@ import string
 class Account(models.Model):
     username = models.CharField(max_length=50, default="", unique=True)
     password = models.CharField(max_length=50, default="")
+
+    def __str__(self):
+        return self.username
+
+class Topic(models.Model):
+    topic = models.CharField(max_length=50, default="", unique=True)
+
+
+    def __str__(self):
+        return self.topic
+
+
+class Post(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, default="")
+    link = models.TextField(default="")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Acc_Topic(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.account + " " + self.topic
